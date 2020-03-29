f4c645273942e618bcc7a70773b40f30ba26ceaa
samir0786 <samirsiescom@gmail.com>
2020-03-29 13:11:07 +0300

    ADDED: Check Part Time Employee Wage

--- employeWage.sh
+++ employeWage.sh
@@ -2,13 +2,20 @@
 
 WAGE_PER_HOURS=20
 FULL_DAY_HOURS=8
-readNumber=$(( RANDOM%2 ))
+PART_TIME_HOURS=4
+readNumber=$(( RANDOM%3 ))
 if [ $readNumber -eq 1 ]
 then
-	echo "Employee is Present"
-	dailyWage=$((WAGE_PER_HOURS*FULL_DAY_HOURS))
+		echo "Employee is Present"
+		dailyWage=$((WAGE_PER_HOURS*FULL_DAY_HOURS))
 else
-	echo "Employee is Absent"
-	dailyWage=0
+		if [ $readNumber -eq 2 ]
+		then
+			dailyWage=$((WAGE_PER_HOURS*PART_TIME_HOURS))
+		else
+			echo "Employee is Absent"
+			dailyWage=0
+		fi
 fi
 echo "Employee Wage: "$dailyWage
+
