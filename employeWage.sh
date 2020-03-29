85fab64f6f747496516a6c26fcf6395083a3a8b1
samir0786 <samirsiescom@gmail.com>
2020-03-29 11:32:35 +0300

    ADDED: Check Daily Employee Wage

--- employeWage.sh
+++ employeWage.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 
+WAGE_PER_HOURS=20
+FULL_DAY_HOURS=8
 readNumber=$(( RANDOM%2 ))
 if [ $readNumber -eq 1 ]
 then
 	echo "Employee is Present"
+	dailyWage=$((WAGE_PER_HOURS*FULL_DAY_HOURS))
 else
 	echo "Employee is Absent"
+	dailyWage=0
 fi
+echo "Employee Wage: "$dailyWage
