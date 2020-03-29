a1ca1638f913d20cca835c5f97fe564f21c1b581
samir0786 <samirsiescom@gmail.com>
2020-03-29 19:01:57 +0300

    ADDED: Calculating Daily Wage and Total Wage

--- employeWage.sh
+++ employeWage.sh
@@ -1,32 +1,38 @@
 #!/bin/bash -x
 
 WAGE_PER_HOUR=20
-FULL_TIME_HOUR=8
-PART_TIME_HOUR=4
 DAY_PER_MONTH=20
 TOTAL_WORKING_HOURS=100
-dailyWage=0
 countWorkingDay=0
-countTotalHours=0
+countTotalHour=0
+getHours=0
+totalWage=0
+
+function getWorkingHours() {
+	case $(( RANDOM%2 )) in
+		1)
+			employeeHours=8
+			;;
+		0)
+			employeeHours=4
+			;;
+	esac
+	echo $employeeHours
+}
+
 while [[ $countTotalHours -le $TOTAL_WORKING_HOURS && $countWorkingDay -ne $DAY_PER_MONTH ]]
 do
 	readNumber=$(( RANDOM%2 ))
 	if [ $readNumber -eq 1 ]
 	then
 		countWorkingDay=$(( countWorkingDay+1 ))
-		case $(( RANDOM%2 )) in
-			1)
-				dailyWage=$(( dailyWage+(FULL_TIME_HOUR*WAGE_PER_HOUR) ))
-				countTotalHours=$(( countTotalHours+FULL_TIME_HOUR ))
-				;;
-			0)
-				dailyWage=$(( dailyWage+(PART_TIME_HOUR*WAGE_PER_HOUR) ))
-				countTotalHours=$(( countTotalHours+PART_TIME_HOUR ))
-				;;
-		esac
+		getHours="$( getWorkingHours )"
+		countTotalHours=$(( countTotalHours+getHours ))
+		arr[$countWorkingDay]=$(( WAGE_PER_HOUR*getHours ))
+		totalWage=$(( totalWage+arr[$countWorkingDay] ))
 	else
-		dailyWage=$(( dailyWage+0 ))
+		totalWage=$(( totalWage+0 ))
 	fi
 done
-echo "Employee Wage : " $dailyWage
-
+echo "Employee Per Day Wage : " ${arr[@]}
+echo "Employee Total Wage : " $totalWage
