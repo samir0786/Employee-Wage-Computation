a6afc9b5fbab0877b7c5f7e496cfcd59218d24dc
samir0786 <samirsiescom@gmail.com>
2020-03-29 13:32:56 +0300

    ADDED: Check Employee Wage Using Case Statement

--- employeWage.sh
+++ employeWage.sh
@@ -3,19 +3,22 @@
 WAGE_PER_HOURS=20
 FULL_DAY_HOURS=8
 PART_TIME_HOURS=4
-readNumber=$(( RANDOM%3 ))
+readNumber=$(( RANDOM%2 ))
 if [ $readNumber -eq 1 ]
 then
-		echo "Employee is Present"
-		dailyWage=$((WAGE_PER_HOURS*FULL_DAY_HOURS))
+	echo "Employee is Present"
+	case $(( RANDOM%2 )) in
+		1)
+			dailyWage=$(( WAGE_PER_HOURS*FULL_DAY_HOURS ))
+			;;
+		0)
+			dailyWage=$(( WAGE_PER_HOURS*PART_TIME_HOURS ))
+			;;
+	esac
+
 else
-		if [ $readNumber -eq 2 ]
-		then
-			dailyWage=$((WAGE_PER_HOURS*PART_TIME_HOURS))
-		else
-			echo "Employee is Absent"
-			dailyWage=0
-		fi
+		echo "Employee is Absent"
+		dailyWage=0
 fi
 echo "Employee Wage: "$dailyWage
 
